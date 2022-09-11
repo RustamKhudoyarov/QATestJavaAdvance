597f2d323c31311e774b60ee5c88257af7d2a1d0
Rustam <khrust_lord@mail.ru>
2022-09-11 01:35:16 -0500

    Add classes App, TestSentence and text file Readme

---
+++ src/App.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class App {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        TestSentence testSentence = new TestSentence();
+        String sentencesForMapWordsDuplicates ="Необходимо переменными создать"
+                + " класс с тремя методами ?с тремя новыми переменными.";
+        testSentence.setInputText(sentencesForMapWordsDuplicates);
+        Map<String , Integer> mapWords ;
+        List<String> sentenceByTheWord;
+
+        mapWords = testSentence.getCountWordsInSentenceMap(sentencesForMapWordsDuplicates);
+        for(Map.Entry<String, Integer> mapWord : mapWords.entrySet()){
+            System.out.println(mapWord.getKey() + ": " + mapWord.getValue());
+        }
+        System.out.println("+++++++++++Search by the word+++++++++++++");
+        System.out.println("Please, you should  enter the word for searching sentences where contains your word:");
+        String searchWord = scanner.next();
+        TestSentence searchWordInText = new TestSentence();
+        String textForSearchWord ="Необходимо создать класс с тремя методами."
+                + " Снова создать новую программу. Решить уравнение.";
+        sentenceByTheWord =  searchWordInText.returnSentencesByWord(searchWord, textForSearchWord);
+        for(String sentence : sentenceByTheWord){
+            System.out.println(sentence);
+        }
+
+    }
+}
---
+++ src/TestSentence.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.lang.*;
+
+public class TestSentence {
+    private String inputText;
+    private Map<String,Integer> mapWords;
+
+
+
+    public void setInputText(String inputText) {
+        this.inputText = inputText;
+    }
+
+    public Map<String, Integer> getCountWordsInSentenceMap(String inputText) {
+      List<String> words = List.of(inputText.split("[, ?.@]+"));
+      mapWords = new HashMap<>();
+      for(String word : words) {
+          Integer count = mapWords.get(word);
+         if(count==null){
+             mapWords.put(word, 1);
+         }else{
+             mapWords.remove(word);
+             mapWords.put(word,count+1);
+         }
+
+      }
+        return mapWords;
+    }
+
+        public List<String> returnSentencesByWord(String word,String inputText){
+        List <String> sentences = List.of(inputText.split("[.]",0)) ;
+        List<String> sentencesContainsWord=new ArrayList<>();
+        for (String sentence : sentences){
+          if(sentence.contains(word)){
+                sentencesContainsWord.add(sentence);
+           }
+
+        }
+       return sentencesContainsWord;
+
+    }
+}
+
+
